b2f38b788f2cd6521fd0b00d6bca4293819dd55c
van-kevin <vankevinsuy@gmail.com>
2024-01-14 11:52:35 -0500

    from nginx to fastapi

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,12 @@
-FROM nginx:alpine
-COPY index.html /usr/share/nginx/html/index.html
-COPY nginx.conf /etc/nginx/nginx.conf
-EXPOSE 80
+FROM python:3.11.4-slim
+
+WORKDIR /code
+
+COPY api.py .
+COPY requirements.txt .
+
+RUN pip install --no-cache-dir --upgrade -r requirements.txt
+
+EXPOSE 5000
+
+CMD ["uvicorn", "api:app", "--host", "0.0.0.0", "--port", "5000"]
---
+++ api.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI, UploadFile
+from fastapi.responses import RedirectResponse
+from google.cloud import storage
+
+
+app = FastAPI()
+BUCKET_NAME = 'auto-srt-new-file'
+
+
+@app.get("/")
+async def redirect_typer():
+    return RedirectResponse("/docs")
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile):
+
+    async def upload_blob(bucket_name, file_to_upload:UploadFile):
+        """Uploads a file to the bucket."""
+
+        storage_client = storage.Client()
+        bucket = storage_client.bucket(bucket_name)
+
+        data_as_bytes = await(file_to_upload.read())
+        blob = bucket.blob(file_to_upload.filename)
+        blob.upload_from_string(data_as_bytes)
+
+        return(
+            f"File {file_to_upload.filename} uploaded to {bucket_name}."
+        )
+
+    res = await upload_blob(BUCKET_NAME,file)
+
+    return res
+
